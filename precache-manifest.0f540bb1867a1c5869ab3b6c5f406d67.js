05418a0674e2eaf0ab358db4a582ca0b2759dc64
Tejaswi Manivannan <tejaswimhtp@gmail.com>
2021-06-04 19:28:49 +0400

    Updates

---
+++ precache-manifest.0f540bb1867a1c5869ab3b6c5f406d67.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3481ad8f12fc79ae98991253a3dd8dd",
+    "url": "http//Tess-Vanta.github.io/ToDoList_React/index.html"
+  },
+  {
+    "revision": "b7fc2c4f1828e728689d",
+    "url": "http//Tess-Vanta.github.io/ToDoList_React/static/js/2.04b38954.chunk.js"
+  },
+  {
+    "revision": "37dde1d390f6fbba3064",
+    "url": "http//Tess-Vanta.github.io/ToDoList_React/static/js/main.fe9f06e0.chunk.js"
+  },
+  {
+    "revision": "5a972cc0b719dbeb354c",
+    "url": "http//Tess-Vanta.github.io/ToDoList_React/static/js/runtime-main.1f2f92db.js"
+  }
+]);
